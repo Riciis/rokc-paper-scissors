c1fd6211267da8b2941e42c2c6316fd6f29921c6
Riciis <114050482+Riciis@users.noreply.github.com>
2022-09-21 09:38:57 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
 import random
 
+
 def script():
   player1 = input("Player 1, please enter your name: ")
   print("Please, choose one of three: rock, paper, scissors")
   player1_figure = input(player1 + ", " + "enter your figure: ")
   figure = ["paper", "rock", "scissors"]
   computer_figure = random.choice(figure)
-
+  print("PC IS " + computer_figure)
   if player1_figure == computer_figure:
     print("Draw")
-  
   elif player1_figure == "rock" and computer_figure == "scissors":
     print(player1 + ", you win")
   elif player1_figure == "scissors" and computer_figure == "paper":
