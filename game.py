8c8cfbdecce7a56bf876792404dd93f096df89a3
Riciis <114050482+Riciis@users.noreply.github.com>
2022-09-21 09:06:39 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,27 @@
+import random
+
+def script():
+  player1 = input("Player 1, please enter your name: ")
+  print("Please, choose one of three: rock, paper, scissors")
+  player1_figure = input(player1 + ", " + "enter your figure: ")
+  figure = ["paper", "rock", "scissors"]
+  computer_figure = random.choice(figure)
+
+  if player1_figure == computer_figure:
+    print("Draw")
+  
+  elif player1_figure == "rock" and computer_figure == "scissors":
+    print(player1 + ", you win")
+  elif player1_figure == "scissors" and computer_figure == "paper":
+    print(player1 + ", you win")
+  elif player1_figure == "paper" and computer_figure == "rock":
+    print(player1 + ", you win")
+  else:
+    print(player1 + ", you lose")
+  
+  restart = input("Would you like to play again? ")
+  if restart == "yes" or restart == "y":
+    script()
+  if restart == "n" or restart == "no":
+    print("Good bye!")
+script()
